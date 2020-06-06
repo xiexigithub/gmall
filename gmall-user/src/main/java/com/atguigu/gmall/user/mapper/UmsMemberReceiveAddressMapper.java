9bdb06bfa8bb31a5560ac0423cc868bd23e7f3a3
zhangsan <zhangsan@itcast.cn>
2020-06-06 21:16:20 +0800

    提交demos

---
+++ gmall-user/src/main/java/com/atguigu/gmall/user/mapper/UmsMemberReceiveAddressMapper.java
@@ -0,0 +1,10 @@
+package com.atguigu.gmall.user.mapper;
+
+import com.atguigu.gmall.bean.UmsMemberReceiveAddress;
+import org.springframework.stereotype.Repository;
+import tk.mybatis.mapper.common.Mapper;
+@Repository
+public interface UmsMemberReceiveAddressMapper extends Mapper<UmsMemberReceiveAddress> {
+
+
+}
